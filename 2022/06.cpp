088aad8e183dfff90218f32e70e245c470aad918
Kolleck, Martin <kolleck@mps.mpg.de>
2022-12-06 17:56:48 +0100

    Solution for 2022-12-06.

---
+++ 2022/06.cpp
@@ -0,0 +1,42 @@
+// https://adventofcode.com/2022/day/6
+
+#include <iostream>
+#include <set>
+
+static bool distinct(const std::string& m)
+{
+    std::set<char> s;
+
+    for (auto c : m)
+    {
+        s.insert(c);
+    }
+
+    return s.size() == m.length();
+}
+
+static void process(const std::string& line, int l)
+{
+    for (int i = l; i < line.length(); i++)
+    {
+        if (distinct(line.substr(i - l, l)))
+        {
+            std::cout << i << std::endl;
+            break;
+        }
+    }
+}
+
+int main(void)
+{
+    std::string line;
+
+    while (!std::getline(std::cin, line).eof())
+    {
+        process(line, 4);
+        process(line, 14);
+        std::cout << "---" << std::endl;
+    }
+
+    return 0;
+}
